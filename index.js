580bb07960a21d5acaacbf0ce77f5f8eac63be1a
mio kataoka <mio.kanna@gmail.com>
2022-03-31 19:37:08 -0700

    commit tree 3a600ce62bf9fe01876306549b43a72c0f330601

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    get circumference() {
+        return Math.PI * (this.radius + this.radius);
+    }
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+    set diameter(val){
+        this.radius = val/2;
+    }
+    set circumference(val){
+        this.radius = (val/Math.PI)/2;
+    }
+    set area(val){
+        this.radius = Math.sqrt(val/Math.PI);
+    }
+}
